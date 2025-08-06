cb9f72daac9e86ca0ce085da40e6dee5a1f4768e
Niccolò Belli <niccolo.belli@linuxsystems.it>
2025-08-06 08:47:40 +0200

    Initial commit

---
+++ src/pages/suppliers/[id].vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Test</div>
+</template>
+<script setup lang="ts">
+  const route = useRoute()
+  console.log(route.params.id)
+</script>
---
+++ src/pages/suppliers/[id]/results/[[version]].vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Test</div>
+</template>
+
+<script setup lang="ts">
+  const route = useRoute()
+  console.log(route.params.id)
+</script>
